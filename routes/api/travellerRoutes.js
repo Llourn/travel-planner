ec893d6e836336a93f4fcb5de78c598d157388ba
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-04-12 11:46:59 -0300

    completed remaining endpoints for traveller

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Traveller, Location } = require("../../models");
+const { Traveller } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -11,18 +11,54 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log(req.method, req.originalUrl);
-  res.json(req.method + ", " + req.originalUrl);
+  try {
+    const travellerData = await Traveller.create(req.body);
+    res.status(200).json({
+      message: "Traveller created successfully!",
+      traveller: travellerData,
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .json({ message: "Failed to create new traveller", error: err });
+  }
 });
 
 router.get("/:id", async (req, res) => {
-  console.log(req.method, req.originalUrl);
-  res.json(req.method + ", " + req.originalUrl);
+  try {
+    const travellerData = await Traveller.findByPk(req.params.id);
+
+    if (!travellerData) {
+      res.status(404).json({ message: "No traveller found with that id!" });
+      return;
+    }
+
+    res
+      .status(200)
+      .json({ message: "Traveller found!", traveller: travellerData });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  console.log(req.method, req.originalUrl);
-  res.json(req.method + ", " + req.originalUrl);
+  try {
+    const travellerData = await Traveller.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!travellerData) {
+      res.status(404).json({ message: "No traveller found with that id!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Traveller deleted successfully!" });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to delete traveller", error: err });
+  }
 });
 
 module.exports = router;
